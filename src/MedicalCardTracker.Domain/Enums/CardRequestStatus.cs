ef6591bfeeddfd9cb72f26e47d65fda646ff24ff
HardManDev <hardman.dev@protonmail.com>
2023-04-04 09:48:42 +0300

    Created CardRequest entity

---
+++ src/MedicalCardTracker.Domain/Enums/CardRequestStatus.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+namespace MedicalCardTracker.Domain.Enums;
+
+public enum CardRequestStatus
+{
+    Canceled = -2,
+    NotCompleted = -1,
+    Created = 0,
+    Completed = 1
+}
