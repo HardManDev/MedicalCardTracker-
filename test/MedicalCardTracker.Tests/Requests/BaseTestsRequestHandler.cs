bb701411b833ca704875bab3c5c86fa664f26b86
HardManDev <hardman.dev@protonmail.com>
2023-04-05 12:52:35 +0300

    Created base tests handler for unit tests

---
+++ test/MedicalCardTracker.Tests/Requests/BaseTestsRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using AutoMapper;
+using MedicalCardTracker.Domain.Entities;
+using MedicalCardTracker.Server.Application.Interfaces;
+using MedicalCardTracker.Tests.Fixtures;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCardTracker.Tests.Requests;
+
+public abstract class BaseTestsRequestHandler
+{
+    protected BaseTestsRequestHandler(IApplicationDbContext dbContext, IMapper mapper)
+    {
+        Mapper = mapper;
+        DbContext = dbContext;
+
+        ((DbContext)DbContext).Database.EnsureCreated();
+        DbContext.CardRequests.AddRange(new List<CardRequest>
+        {
+            FixtureCardRequests.FixtureCardRequest,
+            FixtureCardRequests.FixtureCardRequestForDelete,
+            FixtureCardRequests.FixtureCardRequestForUpdate
+        });
+        ((DbContext)DbContext).SaveChanges();
+    }
+
+    protected IApplicationDbContext DbContext { get; }
+    protected IMapper Mapper { get; }
+}
