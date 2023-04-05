3868e3aa8fff101b8a7ab9918969f003bf361192
HardManDev <hardman.dev@protonmail.com>
2023-04-05 10:26:10 +0300

    Created CardRequest MediatR requests

---
+++ src/MedicalCardTracker.Application/Commands/CardRequests/CreateCardRequest/CreateCardRequestCommand.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using MediatR;
+using MedicalCardTracker.Application.Models.ViewModels;
+using MedicalCardTracker.Domain.Enums;
+
+namespace MedicalCardTracker.Application.Commands.CardRequests.CreateCardRequest;
+
+public class CreateCardRequestCommand : IRequest<CardRequestVm>
+{
+    public string CustomerName { get; set; } = null!;
+    public string TargetAddress { get; set; } = null!;
+
+    public string PatientFullName { get; set; } = null!;
+    public DateOnly? PatientBirthDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public CardRequestStatus Status { get; set; }
+    public CardRequestPriority Priority { get; set; }
+}
