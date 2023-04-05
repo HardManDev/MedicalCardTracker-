5273150d974d1ee6d45db1eb9857d070f6df3e1b
HardManDev <hardman.dev@protonmail.com>
2023-04-05 10:34:27 +0300

    Created application database context interface

---
+++ src/MedicalCardTracker.Server.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using MedicalCardTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalCardTracker.Server.Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<CardRequest> CardRequests { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
