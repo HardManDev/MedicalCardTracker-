adc02244c9b8a806a690173f3ca92256ae135d99
HardManDev <hardman.dev@protonmail.com>
2023-04-05 11:17:13 +0300

    Created dependency injection logic for shared application layer

---
+++ src/MedicalCardTracker.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2023 Mikulchik Vladislav Alekseevich <hardman.dev@pm.me>.
+// This software is licensed under the MIT license.
+// Please see the LICENSE file for more information.
+
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicalCardTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
