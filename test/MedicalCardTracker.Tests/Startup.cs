e5d3e32e96ba86e65923a310da30a10c2610a0d6
HardManDev <hardman.dev@protonmail.com>
2023-04-08 18:47:23 +0300

    Fixed MediatR dependency injection logic

--- test/MedicalCardTracker.Tests/Startup.cs
+++ test/MedicalCardTracker.Tests/Startup.cs
@@ -3,7 +3,6 @@
 // Please see the LICENSE file for more information.
 
 using System.Reflection;
-using MedicalCardTracker.Application;
 using MedicalCardTracker.Application.Interfaces;
 using MedicalCardTracker.Application.Mappings;
 using MedicalCardTracker.Database;
@@ -17,8 +16,6 @@ public static class Startup
 {
     public static void ConfigureServices(IServiceCollection services)
     {
-        services.AddApplication();
-
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
 
